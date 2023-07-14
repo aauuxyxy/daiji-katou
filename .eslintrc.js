33e815c2ada53323fdf2e87b1e53007f3174e8fd
uuaaxyxy <uuaaxyxy968@gmail.com>
2023-07-15 01:37:22 +0900

    🐛 eslint変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-  overrides: [],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-  },
-  settings: {
-    react: {
-      version: '18.2.0',
-    },
   },
 };
