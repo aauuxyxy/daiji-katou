09d1b8da152c0158eacf962bb6d4fa8d62d66aba
aauuxyxy <48760596+aauuxyxy@users.noreply.github.com>
2025-11-06 22:18:28 +0900

    利用技術更新

--- src/pages/components/About.tsx
+++ src/pages/components/About.tsx
@@ -24,7 +24,7 @@ const About = () => {
     },
     {
       label: '利用技術',
-      value: 'React/Node.js/Express',
+      value: 'TypeScript/React/Express',
       svg: 'M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z',
       description:
         'これまでTypeScriptをメインに開発を行っておりました。また、フロントエンドでは主にReactやNext.js、バックエンドではExpressを主として活用しておりました。',
